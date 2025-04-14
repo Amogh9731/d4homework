58c53ad1336c65baee54804b54e891d3c1860d2c
Amogh9731 <amoghnaik28@gmail.com>
2025-04-14 23:34:22 +0530

    files added

---
+++ my-app/src/Movie.jsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+import Movies from './MovieData.json'
+import MovieCard from './MovieCard';
+
+function Movie(){
+    return (
+        <>
+        <div className="movie-container">
+        {Movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+        </>
+    );
+}
+
+export default Movie;
